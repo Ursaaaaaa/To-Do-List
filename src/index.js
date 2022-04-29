12b5409cd2e30e0eb651707a3e6178d099f1d184
ramrod433 <djoletoamanor@gmail.com>
2022-04-29 13:43:24 +0000

    Added new JS for and and remove function

--- src/index.js
+++ src/index.js
@@ -1,64 +1,16 @@
 import './index.css';
+import List from './todo.js';
 
-const toDoList = [
-  {
-    description: 'Wash Car',
-    completed: true,
-    index: 0,
-  },
+const todolist = new List();
+todolist.display();
 
-  {
-    description: 'Join Shai Conference',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Setup Project Tools',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Buy Groceries',
-    completed: false,
-    index: 3,
-  },
-];
+document.querySelector('#form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const activity = e.target.elements.activity.value;
+  todolist.addWork(activity);
+  e.target.reset();
+});
 
-const list = document.querySelector('.listed-items');
-list.innerHTML = '';
-for (let i = 0; i < toDoList.length; i += 1) {
-  const work = toDoList[i];
-  let workItem = `
-      <li class=" list-items">`;
-  if (work.completed) {
-    workItem += `<span class="material-icons done" onclick="updateStatus(${work.index}, 'pending')">
-            done
-          </span>
-          <p contenteditable="true" class="completed">
-            ${work.description}
-          </p>
-          `;
-  } else {
-    workItem += `<span class="material-icons check-box" onClick ="UpdateStatus(${work.index}, 'completed')">
-                  check_box_outline_blank
-                  </span>
-                  <p  contenteditable="true">
-                  ${work.description}
-                  </P>`;
-  }
-  workItem += `
-    <span class="material-icons more-bars">
-         more_vert
-      </span>
-     <!-- <span class="material-icons" onclick="deleteTask(${work.index})">
-          delete
-        </span> -->
-     </li>`;
-  list.innerHTML += workItem;
-}
-
-toDoList.sort((a, b) => {
-  if (a.index < b.index) return -1;
-  if (a.index > b.index) return 1;
-  return 0;
+document.querySelector('#delete-all').addEventListener('click', () => {
+  todolist.clearAll();
 });
